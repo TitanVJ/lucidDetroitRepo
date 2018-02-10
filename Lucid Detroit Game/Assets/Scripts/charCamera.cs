9fd2492f7231f5eda06778a3913ece6c3d92a127
DusanKovac <36318541+DusanKovac@users.noreply.github.com>
2018-02-10 11:26:39 -0800

    Add files via upload
    Character Camera follow

---
+++ Lucid Detroit Game/Assets/Scripts/charCamera.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class charCamera : MonoBehaviour {
+
+
+    public float cameraSpeed;
+    public GameObject player;
+    public Vector3 offsetRight;
+    public Vector3 offsetLeft;
+    private Vector3 offsetY;
+    Vector3 targetPos;
+   
+
+    void Start()
+    {
+        offsetRight = transform.position - player.transform.position;
+        offsetLeft = transform.position + player.transform.position;
+        targetPos = transform.position;
+        offsetY = new Vector3(0f, 1.39f, 0f);
+    }
+
+    void FixedUpdate()
+    {
+        if (player && Input.GetKey(KeyCode.D))
+        {
+            Vector3 targetCamPos = player.transform.position + offsetRight;
+
+            transform.position = Vector3.Lerp(transform.position, targetCamPos, 2f * Time.deltaTime)        ;
+        }
+        else if (player && Input.GetKey(KeyCode.A))
+        {
+            Vector3 targetCamPos = player.transform.position + offsetLeft + 2*offsetY;
+
+            transform.position = Vector3.Lerp(transform.position, targetCamPos, 2f * Time.deltaTime);
+        }
+    }
+}
