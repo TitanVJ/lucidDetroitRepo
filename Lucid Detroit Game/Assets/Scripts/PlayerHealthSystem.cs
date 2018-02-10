59f27ea571e2fb33c2d012f5ba3b4d3834bc365e
TitanVJ <Vijenderbakhshi@gmail.com>
2018-02-10 11:05:35 -0800

    Added player health system. Still untested so pls no complain.

---
+++ Lucid Detroit Game/Assets/Scripts/PlayerHealthSystem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthSystem : MonoBehaviour {
+    public int initHealth = 100;
+    public int currentHealth;
+        public Slider healthBar;
+    //need to add assets for death
+
+
+    bool isDead;
+    bool isDamaged;
+
+    PlayerMove playerMove;
+
+	// Use this for initialization
+	void Start () {
+        currentHealth = initHealth;
+        playerMove = GetComponent<PlayerMove>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void takeDamage(int dmgAmnt)
+    {
+        isDamaged = true;
+        currentHealth -= dmgAmnt;
+        healthBar.value = currentHealth;
+
+        if (currentHealth <= 0 && !isDead)
+            death();
+
+    }
+    void death()
+    {
+        isDead = true;
+        //death anim and sounds
+
+        playerMove.enabled = false;
+    }
+}
