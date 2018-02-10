a1b8aeeca38f716e2ec7ad17b6b7485b2caf67bf
TitanVJ <Vijenderbakhshi@gmail.com>
2018-02-10 06:18:30 -0800

    Finished black and white version of some of the alchohol buggs

--- Lucid Detroit Game/Assets/Scripts/PlayerMove.cs
+++ Lucid Detroit Game/Assets/Scripts/PlayerMove.cs
@@ -8,25 +8,34 @@ public class PlayerMove : MonoBehaviour
     public float moveSpeed = 5.0f;
     public float jumpForce = 10.0f;
     private bool canJump = true;
+    private SpriteRenderer mySpriteRenderer;
+    public Transform spawnPoint;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start ()
     {
         if(rb == null)
             rb = GetComponent<Rigidbody2D>();
         rb.freezeRotation = true;
-	}
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
 		if(Input.GetKey(KeyCode.A))
         {
+            
             transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+            mySpriteRenderer.flipX = true;
+
         }
         if (Input.GetKey(KeyCode.D))
         {
+            mySpriteRenderer.flipX = false;
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+
         }
         
         if(Input.GetKey(KeyCode.W) && canJump)
