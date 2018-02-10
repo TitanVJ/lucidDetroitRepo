94a7f44cc4e47580bc2a933a5ea7b65c346ebaf9
Alex <alexanderbevislee@gmail.com>
2018-02-09 21:38:23 -0800

    Made shooting script, it is currently broken, if you want to use this, just disable the shooting script.

---
+++ Lucid Detroit Game/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject bullet;
+    public float bulletSpeed = 50.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetKey(KeyCode.Space))
+        {
+            Instantiate(bullet, transform.position);
+        }
+	}
+}
